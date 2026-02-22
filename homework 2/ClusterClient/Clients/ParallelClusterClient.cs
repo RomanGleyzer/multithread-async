d7d401f5dd44dc38a8b17ae8688ea433d46b413d
Roman <romy4.gleyzer@mail.ru>
2026-02-22 11:15:02 +0500

    Parallel (1 балл)
    Ждем ответа хотя бы от одной реплики и сразу же возвращаем ответ

--- homework 2/ClusterClient/Clients/ParallelClusterClient.cs
+++ homework 2/ClusterClient/Clients/ParallelClusterClient.cs
@@ -1,23 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using log4net;
 
-namespace ClusterClient.Clients
+namespace ClusterClient.Clients;
+
+public class ParallelClusterClient(string[] replicaAddresses) : ClusterClientBase(replicaAddresses)
 {
-    public class ParallelClusterClient : ClusterClientBase
+    public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
     {
-        public ParallelClusterClient(string[] replicaAddresses) : base(replicaAddresses)
+        if (ReplicaAddresses == null || ReplicaAddresses.Length == 0)
+            throw new InvalidOperationException("Реплика адресов не указана");
+
+        var tasks = new Task<string>[ReplicaAddresses.Length];
+
+        for (var i = 0; i < ReplicaAddresses.Length; i++)
         {
+            var uri = ReplicaAddresses[i];
+            var webRequest = CreateRequest(uri + "?query=" + query);
+
+            Log.InfoFormat($"Обработка {webRequest.RequestUri}");
+
+            tasks[i] = ProcessRequestAsync(webRequest);
         }
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
-        {
-            throw new NotImplementedException();
-        }
+        var anyReplicaTask = Task.WhenAny(tasks);
+        var timeoutTask = Task.Delay(timeout);
 
-        protected override ILog Log => LogManager.GetLogger(typeof(ParallelClusterClient));
+        var completed = await Task.WhenAny(anyReplicaTask, timeoutTask);
+        if (ReferenceEquals(completed, timeoutTask))
+            throw new TimeoutException();
+
+        return await anyReplicaTask.Result;
     }
+
+    protected override ILog Log => LogManager.GetLogger(typeof(ParallelClusterClient));
 }
